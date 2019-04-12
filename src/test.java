c6bb4b943486b8f4516891e2c641fb849d585158
sc <584352451@qq.com>
2019-04-12 21:24:01 +0800

    a

--- src/test.java
+++ src/test.java
@@ -2,6 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+
           System.out.println(2);
+
+        System.out.println(3);
+
     }
 }
