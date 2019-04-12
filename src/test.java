e1083b396291b50e360a72d6e2c9639703c86e66
sc <584352451@qq.com>
2019-04-12 21:29:22 +0800

    a

--- src/test.java
+++ src/test.java
@@ -2,10 +2,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-
           System.out.println(2);
-
         System.out.println(3);
+        System.out.println("dev");
 
     }
 }
